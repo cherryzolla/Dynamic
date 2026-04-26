fec267fe6847b5c7b228aeb7f4866913f2e4d5af
cherryzolla <cherryxzolla@gmail.com>
2026-04-26 07:23:45 +0000

    export...export was the problem

--- auth.js
+++ auth.js
@@ -1,10 +1,7 @@
-
-
 // You get these from your Supabase Dashboard -> Settings -> API
 const supabaseUrl = 'https://wgxszhetpietfjlgeffp.supabase.co'
 const supabaseKey = 'sb_publishable_3NhlyfBqrV1fI3g_-TbMBA_NkXH8JRs'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 // This makes sure your buttons in index.html can "see" these functions
 window.login = login;
 window.register = register;
